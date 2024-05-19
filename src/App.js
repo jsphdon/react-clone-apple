ddbf255b209cc84941a6f5f7c13003601d03ca72
jsphdon <jdcagampang14@gmail.com>
2024-05-19 16:40:09 +0400

    able to run tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './index.css';
 
 function App() {
   return (
-    <h1 className='text-sm'>Hello World</h1>
+    <h1 className='underline'>Hello World</h1>
   );
 }
 
